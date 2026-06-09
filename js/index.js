2e273b6b5b61dc5fe2780b26d8029dc8da9d9d43
Tinashe Toto <ttoto@my.bcit.ca>
2026-06-08 18:16:36 -0700

    add portfolio

---
+++ js/index.js
@@ -0,0 +1,99 @@
+// Skills
+const skills = [
+  "Java",
+  "Python",
+  "SQL",
+  "Web Markup Languages: HTML, CSS",
+  "Linux",
+  "Networking Fundamentals",
+];
+
+const skillsList = document.querySelector("#skills .list-group");
+
+skills.forEach((skill) => {
+  const li = document.createElement("li");
+  li.className = "list-group-item";
+  li.textContent = skill;
+  skillsList.appendChild(li);
+});
+
+// Projects
+const projects = [
+  {
+    title: "SunBlock",
+    description: "A web app that helps users find shade during peak UV hours.",
+    details: `
+        SunBlock is a web app designed to help users find shaded areas during peak UV hours.
+
+        It uses geolocation to provide real-time shade information and suggests nearby shaded locations.
+
+        SunBlock was created by myself, Zyllian James Fran, Harman Kaur, Kelly Bayingana, and Taeu Gim using Bootstrap, Node.js, and MongoDB.
+    `,
+    image: "./images/sunblock.jpg",
+  },
+];
+
+const projectsSection = document.querySelector("#projects");
+
+projects.forEach((project, index) => {
+  projectsSection.innerHTML += `
+    <div class="card" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">${project.title}</h5>
+        <p class="card-text">${project.description}</p>
+        <button
+          class="btn btn-secondary more-info"
+          data-index="${index}">
+          More info
+        </button>
+      </div>
+    </div>
+  `;
+});
+
+// Modal
+const modalElement = document.getElementById("projectModal");
+const modal = new bootstrap.Modal(modalElement);
+const modalImage = document.getElementById("modalImage");
+
+const modalTitle = modalElement.querySelector(".modal-title");
+const modalDescription = document.getElementById("modalDescription");
+
+document.querySelectorAll(".more-info").forEach((button) => {
+  button.addEventListener("click", () => {
+    const project = projects[button.dataset.index];
+
+    modalTitle.textContent = project.title;
+    modalDescription.textContent = project.details;
+    modalImage.src = project.image;
+    modalImage.alt = project.title;
+
+    modal.show();
+  });
+});
+
+// Contact
+const contacts = [
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/tinashetoto",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/nash-ft",
+  },
+  {
+    name: "Email",
+    url: "mailto:ttoto@my.bcit.ca",
+  },
+];
+
+const contactGroup = document.querySelector(".btn-group");
+
+contacts.forEach((contact) => {
+  contactGroup.innerHTML += `
+    <a href="${contact.url}" target="_blank" class="btn btn-secondary btn-lg">
+      ${contact.name}
+    </a>
+  `;
+});
